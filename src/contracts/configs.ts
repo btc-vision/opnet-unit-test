cf18364a6ba7bc42209905eb4a28b2de8ea17a19
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-07-23 03:46:45 -0400

    Upgraded libaries

--- src/contracts/configs.ts
+++ src/contracts/configs.ts
@@ -4,4 +4,4 @@ export const FACTORY_ADDRESS: Address = 'bcrt1qgph2k9dahptnz2vu72ezd9r2p4phjm2pk
 export const POOL_ADDRESS: Address = 'bcrt1q5nkjpjmh6xwweprcyeylwdw7aud4een7p9pphd';
 export const WBTC_ADDRESS: Address = 'bcrt1qefq0lwqwpt5lx7hl2dr2r8q6z063725tccuqrg';
 export const MOTO_ADDRESS: Address = 'bcrt1q8reuxx9naek4mqesrfsgdpjv3q7a5g2llkh6ua';
-export const ROUTER_ADDRESS: Address = 'bcrt1qelqwcguvwkgr90w6u5f2q0a7gqlnq4w8rl26ht';
+export const ROUTER_ADDRESS: Address = 'bcrt1qyx492l440f6cm5hdppw5pmkd2fc7k3qayuuvh6';
