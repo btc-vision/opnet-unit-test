37a99c4dfb7e913f5537749817490661af59bb36
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-08-01 03:15:26 -0400

    update

--- src/contracts/configs.ts
+++ src/contracts/configs.ts
@@ -7,7 +7,7 @@ export const MOTO_ADDRESS: Address = 'bcrt1q8reuxx9naek4mqesrfsgdpjv3q7a5g2llkh6
 export const ROUTER_ADDRESS: Address = 'bcrt1qyx492l440f6cm5hdppw5pmkd2fc7k3qayuuvh6';*/
 
 export const FACTORY_ADDRESS: Address = 'tb1qunf06kfl5uc3apzsxxn9d55790ahnekg8j4m9u';
-export const POOL_ADDRESS: Address = 'tb1q0y6qm6zrm6dzxxdlgwn0ut9tuya8a7ff4uupne';
+export const POOL_ADDRESS: Address = 'tb1q6a7yw353hjmresphupytw5vczpqxtg4yrupayk';
 export const WBTC_ADDRESS: Address = 'tb1q76ghqx24dpwqv4e9dsdhezxxjep52cjrsktafa';
 export const MOTO_ADDRESS: Address = 'tb1q4tyhf8hpu04qjj3qaag20knun0spctultxzakw';
-export const ROUTER_ADDRESS: Address = 'tb1qzyn09v5s5ujpm5zlcqqdz6wjc67qk9sv2wzka9';
+export const ROUTER_ADDRESS: Address = 'tb1q9qcxqgmxrlr02x65avjs2zp5ppwfxz5eq4amrl';
