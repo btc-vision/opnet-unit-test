e048273c177e569867acb3212e27274718d0050a
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-08-01 23:31:22 -0400

    hot fixes

--- src/contracts/configs.ts
+++ src/contracts/configs.ts
@@ -2,12 +2,12 @@ import { Address } from '@btc-vision/bsi-binary';
 
 /*export const FACTORY_ADDRESS: Address = 'bcrt1qgph2k9dahptnz2vu72ezd9r2p4phjm2pkgu7ht';
 export const POOL_ADDRESS: Address = 'bcrt1q5nkjpjmh6xwweprcyeylwdw7aud4een7p9pphd';
-export const WBTC_ADDRESS: Address = 'bcrt1qefq0lwqwpt5lx7hl2dr2r8q6z063725tccuqrg';
+export const WBTC_ADDRESS: Address = 'bcrt1qnra3nd3gng0u7rqj9cpepsg3vfetldc9phvsrd';
 export const MOTO_ADDRESS: Address = 'bcrt1q8reuxx9naek4mqesrfsgdpjv3q7a5g2llkh6ua';
 export const ROUTER_ADDRESS: Address = 'bcrt1qyx492l440f6cm5hdppw5pmkd2fc7k3qayuuvh6';*/
 
 export const FACTORY_ADDRESS: Address = 'tb1qgev5kldhp5zvg6j8t9vl6x4phkrwn8nk9felxh';
 export const POOL_ADDRESS: Address = 'tb1q6a7yw353hjmresphupytw5vczpqxtg4yrupayk';
-export const WBTC_ADDRESS: Address = 'tb1q76ghqx24dpwqv4e9dsdhezxxjep52cjrsktafa';
+export const WBTC_ADDRESS: Address = 'tb1qq89wm9vfzmxla7jlstyz8s3fm40vl589r6mltj';
 export const MOTO_ADDRESS: Address = 'tb1q4tyhf8hpu04qjj3qaag20knun0spctultxzakw';
 export const ROUTER_ADDRESS: Address = 'tb1qre7tlyrxrs3lht6ls6y2wezgq8hlmr78y2j9y2';
