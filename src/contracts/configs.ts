01c8ac19ffe1d2989c84aa325ea9dfff8594566e
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-09-15 19:23:32 -0400

    updated ca address

--- src/contracts/configs.ts
+++ src/contracts/configs.ts
@@ -5,11 +5,11 @@ import { Network, networks } from 'bitcoinjs-lib';
 export const NETWORK: Network = networks.regtest;
 
 // Contract addresses
-export const FACTORY_ADDRESS: Address = 'bcrt1qxtpreq8zg7pp9wm550kjrhaa2r5kj6lhph9he5';
-export const POOL_ADDRESS: Address = 'bcrt1qqg2a8076rwuruzetdyquj8fh5jxtc22pmh9vep';
-export const WBTC_ADDRESS: Address = 'bcrt1qdr7sjgtnudda8zrfklw8l5cnrxum5hns7e46hf';
-export const MOTO_ADDRESS: Address = 'bcrt1q8reuxx9naek4mqesrfsgdpjv3q7a5g2llkh6ua';
-export const ROUTER_ADDRESS: Address = 'bcrt1qplnz54sca73t8a03nh494jatr9ffjg6ecarrj8';
+export const FACTORY_ADDRESS: Address = 'bcrt1q9pf9fnpch9z2qrp5e3dgr2avzu3mypq3km2k40';
+export const POOL_ADDRESS: Address = 'bcrt1qg87nx9v9ln3qyadcn0llekzjn0hx8js46ztwky';
+export const WBTC_ADDRESS: Address = 'bcrt1qy44f5630m4ap4mvmgqc44qh4vndaees9y30t0m';
+export const MOTO_ADDRESS: Address = 'bcrt1q5txqpm5sy0s2xsprvce4ddj0088nlq8lazkd6n';
+export const ROUTER_ADDRESS: Address = 'bcrt1q9yd6mk324k0q4krmlxjky0pk65ul6hkf4u35e6';
 
 // Trace flags
 export const TRACE_GAS: boolean = false;
