0613ac98de95bbd495b970a34084615e54835394
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-09-16 19:04:39 -0400

    Updated to runtime 1.5.6 (updated builder)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-import gulpESLintNew from 'gulp-eslint-new';
 import gulp from 'gulp';
 import gulpcache from 'gulp-cached';
 
-import clean from 'gulp-clean';
+import gulpClean from 'gulp-clean';
+import gulpESLintNew from 'gulp-eslint-new';
 import logger from 'gulp-logger';
 import ts from 'gulp-typescript';
 
@@ -10,69 +10,33 @@ process.on('uncaughtException', function (err) {
     console.log('Caught exception: ', err);
 });
 
-process.on('unhandledRejection', (reason, p) => {
-    //console.error('Unhandled Rejection at:', p, 'reason:', reason);
-});
-
 const tsProject = ts.createProject('tsconfig.json');
 
-function onError(e) {
-    console.log('Errored', e);
+function buildESM() {
+    return tsProject
+        .src()
+        .pipe(gulpcache('ts-esm'))
+        .pipe(
+            logger({
+                before: 'Starting...',
+                after: 'Project compiled!',
+                extname: '.js',
+                showChange: true,
+            }),
+        )
+        .pipe(gulpESLintNew())
+        .pipe(gulpESLintNew.format())
+        .pipe(tsProject())
+        .pipe(gulp.dest('build'));
 }
 
-async function build() {
-    return new Promise((resolve) => {
-        tsProject
-            .src()
-            .on('error', onError)
-            .pipe(gulpcache())
-            .pipe(
-                logger({
-                    before: 'Starting...',
-                    after: 'Project compiled!',
-                    extname: '.js',
-                    showChange: true,
-                }),
-            )
-            .pipe(gulpESLintNew())
-            .pipe(gulpESLintNew.format())
-            .pipe(tsProject())
-            .pipe(gulp.dest('build'))
-            .on('end', async () => {
-                resolve();
-            });
-    });
+export async function clean() {
+    return gulp.src('./build/src', { read: false }).pipe(gulpClean());
 }
 
-async function cleanFiles() {
-    return new Promise((resolve) => {
-        gulp.src('./build/src', { read: false })
-            .pipe(clean())
-            .on('end', async () => {
-                resolve();
-            });
-    });
+export const build = buildESM;
+export default build;
+
+export function watch() {
+    gulp.watch(['src/**/*.ts', 'src/**/*.js'], gulp.series(buildESM));
 }
-
-gulp.task('default', async () => {
-    await build().catch(() => {});
-
-    return true;
-});
-
-gulp.task(`clean`, async () => {
-    await cleanFiles();
-});
-
-gulp.task('watch', () => {
-    gulp.watch(
-        ['src/**/**/*.ts', 'src/**/*.ts', 'src/**/*.js', 'src/*.ts', 'src/*.js'],
-        async (cb) => {
-            await build().catch((e) => {
-                console.log('Errored 2', e);
-            });
-
-            cb();
-        },
-    );
-});
