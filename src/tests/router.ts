1bf23e43b8f4016d9cf79abad5bb3f3e7d22ba48
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-10-19 19:01:53 -0400

    Updated bytecode

--- src/tests/router.ts
+++ src/tests/router.ts
@@ -246,7 +246,7 @@ await opnet('Motoswap Router', async (vm: OPNetUnit) => {
         Assert.expect(amountsOuts).toDeepEqual([2n, 1n]);
     });
 
-    /*await vm.it(`should getAmountsOut - big values`, async () => {
+    await vm.it(`should getAmountsOut - big values`, async () => {
         await approveTokens(MaxUint256, MaxUint256);
 
         await router.addLiquidity({
@@ -262,9 +262,8 @@ await opnet('Motoswap Router', async (vm: OPNetUnit) => {
 
         const path: Address[] = [WBTC_ADDRESS, dttAddress];
         const amountsOuts = await router.getAmountsOut(10000n, path);
-        console.log(amountsOuts);
-        Assert.expect(amountsOuts).toDeepEqual([10000, 138]);
-    });*/
+        Assert.expect(amountsOuts).toDeepEqual([10000n, 138n]);
+    });
 
     await vm.it(`should getAmountsIn`, async () => {
         await approveTokens(MaxUint256, MaxUint256);
